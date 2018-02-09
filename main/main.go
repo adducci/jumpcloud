fadc97b260aec36f045efd098e5b8ebc9a38608f
adducci <adducci@seas.upenn.edu>
2018-02-09 16:56:41 -0700

    inputted port

--- main/main.go
+++ main/main.go
@@ -5,6 +5,10 @@ import (
     "net/http"
     "github.com/adducci/jumpcloud/password"
     "time"
+    "os"
+    "strings"
+    "bufio"
+    "strconv"
 )
 
 type hashHandler struct {}
@@ -35,15 +39,47 @@ func (h hashHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-/*
-Launch HTTP Server that listens to requests for /hash on port 8080
-*/
-func main () {
-	var h hashHandler
-	http.Handle("/hash", h)
-    http.ListenAndServe(":8080", nil)	
+//user chooses port number from 1024 to 49151
+func getPort() string {
+	reader := bufio.NewReader(os.Stdin)
+	var input string
+
+	//continue prompting until valid port entered or q entered to quit
+    for havePort := false; havePort == false ; {
+    	//get input
+    	fmt.Print("Enter port: ")
+        input, _ = reader.ReadString('\n')
+        input = strings.TrimSpace(input)
+
+        //q for quit
+        if input == "q" {
+        	return ""
+        }
+
+        //check if valid
+        port, err := strconv.ParseInt(input, 10, 16)
+        if err != nil  || port < 1024 || port > 49151 {
+    	    fmt.Println("Please enter valid port number from 1024 to 49151 or enter q to quit")
+        } else {
+        	havePort = true
+        }
+    }
+
+    return input
 }
 
-func errorHandle(status int, message string) {
-
+/*
+Launch HTTP Server that listens to requests for /hash on provided port
+*/
+func main () {
+	//get port
+    port := getPort()
+    if port == "" {
+    	return
+    }
+    
+    //listen on port
+	var h hashHandler
+	http.Handle("/hash", h)
+    http.ListenAndServe(":" + port, nil)	
 }
