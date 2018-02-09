c66dea06977e2ece7b1fe24c469317fbbd2886c6
adducci <adducci@seas.upenn.edu>
2018-02-09 15:12:24 -0700

    added lag

--- main/main.go
+++ main/main.go
@@ -4,18 +4,28 @@ import (
     "fmt"
     "net/http"
     "github.com/adducci/jumpcloud/password"
+    "time"
 )
 
 type hashHandler struct {}
 
 
+/*
+Accept posts with a form field named password
+Respond with base64 encoded string of password with SHA512 hash
+Lag for 5 seconds before responding
+*/
 func (h hashHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	//accept Post requests
+	//respon to only Post requests
 	if r.Method == http.MethodPost {
+		//encrypt given password if given
 		r.ParseForm()
-		pw := r.PostForm["password"]
-        fmt.Fprint(w, password.Encrypt(pw[0]))
+		pw, ok := r.Form["password"]
+		if ok {
+            fmt.Fprint(w, password.Encrypt(pw[0]))
+		}
 	}
+	time.Sleep(time.Second * 5)
 }
 
 /*
--- main/main_test.go
+++ main/main_test.go
@@ -7,20 +7,28 @@ import (
    "net/url"
    "io/ioutil"
    "log"
+   "time"
+   "sync"
 )
 
- func TestHashHandlerGoodRequest(t *testing.T) {
+var ts *httptest.Server
+
+func TestMain(m *testing.M) {
+	//setup test server
+	var h hashHandler
+ 	http.Handle("/hash", h)
+  	ts = httptest.NewServer(h)
+  	defer ts.Close()
+
+  	m.Run()
+}
+
+ func TestHashHandlerReturnsCorrectHash(t *testing.T) {
  	cases := []struct {
 		in, want string
 	} {
 		{"angryMonkey", "ZEHhWB65gUlzdVwtDQArEyx+KVLzp/aTaRaPlBzYRIFj6vjFdqEb0Q5B8zVKCZ0vKbZPZklJz0Fd7su2A+gf7Q=="},
 	}
-
- 	var h hashHandler
- 	http.Handle("/hash", h)
-  	ts := httptest.NewServer(h)
-  	defer ts.Close()
-  
     for _, c := range cases {
     	v := url.Values{}
     	v.Set("password", c.in)
@@ -38,4 +46,44 @@ import (
 		}
 	}
 }
+
+ func TestHashHandlerLeavesSocketOpen(t *testing.T) {
+ 	start := time.Now()
+
+	v := url.Values{}
+    v.Set("password", "angryMonkey")
+
+    http.PostForm(ts.URL + "/hash", v)
+
+    duration := time.Since(start).Seconds()
+	if duration < 5 || duration > 6 {
+		t.Errorf("Socket not lagging for 5 seonds")
+	} 
+}
+
+func TestHashHandlerConcurrentRequests(t *testing.T) {
+	start := time.Now()
+
+	var wait sync.WaitGroup
+	wait.Add(2)
+
+	v := url.Values{}
+    v.Set("password", "angryMonkey")
+
+    for i := 0; i < 2; i++ {
+        go func () { 
+            defer wait.Done()
+            http.PostForm(ts.URL + "/hash", v)
+        }()
+    }
+
+    wait.Wait()
+
+    duration := time.Since(start).Seconds()
+	if duration > 6 {
+		t.Errorf("Not handling requests concurrently")
+	} 
+}
+
+
 
